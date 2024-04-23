07d8a62a63dd0b20045d3a72768b33bd98d3fc14
Beboo-stack <81274380+Beboo-stack@users.noreply.github.com>
2024-04-23 07:32:02 +0200

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -9,11 +9,7 @@ const Hero = () => {
       <div className="flex flex-col items-center justify-center lg:flex-row">
         <div className="mb-10 lg:max-w-lg lg:pr-5 lg:mb-0 ">
           <div className="max-w-xl mb-6">
-            <div className="text-center">
-              <p className="inline-block text-center px-3 py-px mb-4 text-sm font-semibold tracking-wider text-white uppercase rounded-full bg-teal-accent-400">
-                Brand new
-              </p>
-            </div>
+            
             <h2 className="max-w-lg text-center mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
               The quick, brown fox
               <br className="hidden md:block" />
