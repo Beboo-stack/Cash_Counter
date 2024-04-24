4a8d5698889607943fd3b93ffb32dafd15c7f54a
lionzak <81274380+Beboo-stack@users.noreply.github.com>
2024-04-24 02:38:05 +0200

    form

--- components/email-template.jsx
+++ components/email-template.jsx
@@ -1,7 +1,82 @@
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Hr,
+  Html,
+  Img,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
 import * as React from "react";
 
-export const EmailTemplate = ({ firstName }) => (
-  <div>
-    <h1>Welcome, {firstName}!</h1>
-  </div>
+export const EmailTemplate = ({ firstName, email, phone, message }) => (
+  <Html>
+    <Head />
+    <Preview>
+      The Ecommerce Platform For Yout Digital Products search now for your
+      future
+    </Preview>
+    <Body style={main}>
+      <Container style={container}>
+        <Img src="/logo.png" width="420" height="300" alt="Logo" style={logo} />
+        <Text style={paragraph}>Hi {firstName},</Text>
+        <Text style={paragraph}>{message}</Text>
+
+        <Text style={paragraph}>
+          Best,
+          <br />
+          The Sleem Tech team
+        </Text>
+        <Hr style={hr} />
+        <Text style={footer}>Subscribe to Ali Sleem</Text>
+      </Container>
+    </Body>
+  </Html>
 );
+
+const main = {
+  backgroundColor: "#ffffff",
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+};
+
+const container = {
+  margin: "0 auto",
+  padding: "20px 0 48px",
+};
+
+const logo = {
+  margin: "0 auto",
+};
+
+const paragraph = {
+  fontSize: "16px",
+  lineHeight: "26px",
+};
+
+const btnContainer = {
+  textAlign: "center",
+};
+
+const button = {
+  backgroundColor: "#5F51E8",
+  borderRadius: "3px",
+  color: "#fff",
+  fontSize: "16px",
+  textDecoration: "none",
+  textAlign: "center",
+  display: "block",
+};
+
+const hr = {
+  borderColor: "#cccccc",
+  margin: "20px 0",
+};
+
+const footer = {
+  color: "#8898aa",
+  fontSize: "12px",
+};
